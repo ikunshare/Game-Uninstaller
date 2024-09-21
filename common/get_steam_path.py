b53a1fbe3f806829bb9d16459e5875b523e5d029
WangXianming <119822522+muwenyan521@users.noreply.github.com>
2024-09-21 11:48:20 +0800

    feat:优化

--- common/get_steam_path.py
+++ common/get_steam_path.py
@@ -1,10 +1,17 @@
 import winreg
-
 from pathlib import Path
 
 def get_steam_path():
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Valve\Steam')
-        steam_path = Path(winreg.QueryValueEx(key, 'SteamPath')[0])
-        return steam_path
-    
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Valve\Steam') as key:
+            steam_path = Path(winreg.QueryValueEx(key, 'SteamPath')[0])
+            return steam_path
+    except FileNotFoundError:
+        print("未找到 Steam 安装路径，请确保 Steam 已安装。")
+        return None
+    except Exception as e:
+        print(f"发生错误: {e}")
+        return None
+
 steam_path = get_steam_path()
+
